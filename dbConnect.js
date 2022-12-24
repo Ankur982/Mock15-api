a0607d5afa86c967b79eec81634a48e84d7dcaec
Ankur982 <singh999ankur@gmail.com>
2022-12-24 16:38:25 +0530

    adding

--- dbConnect.js
+++ dbConnect.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = () => {
     mongoose.set('strictQuery', false);
+    
     mongoose.connect(process.env.MONGO_URL)
         .then(() => {
             console.log("DB connected Successfully TO MongoDB Database")
